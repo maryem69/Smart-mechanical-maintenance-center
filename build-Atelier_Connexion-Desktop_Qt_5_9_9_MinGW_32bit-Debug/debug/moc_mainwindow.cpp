cf9e13081149d7e9c5c4a4aaf32e0cf7f9736d16
Aymen <aymen.raddedi@esprit.tn>
2024-11-16 21:39:59 +0100

    some minor updates

--- build-Atelier_Connexion-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-Atelier_Connexion-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[175];
+    QByteArrayData data[15];
+    char stringdata0[318];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,14 +38,25 @@ QT_MOC_LITERAL(3, 42, 31), // "on_pushButton_supprimer_clicked"
 QT_MOC_LITERAL(4, 74, 28), // "on_pushButton_update_clicked"
 QT_MOC_LITERAL(5, 103, 28), // "on_pushButton_search_clicked"
 QT_MOC_LITERAL(6, 132, 12), // "extractToPdf"
-QT_MOC_LITERAL(7, 145, 29) // "on_pushButton_refresh_clicked"
+QT_MOC_LITERAL(7, 145, 29), // "on_pushButton_refresh_clicked"
+QT_MOC_LITERAL(8, 175, 30), // "on_pushButton_search_2_clicked"
+QT_MOC_LITERAL(9, 206, 12), // "addToHistory"
+QT_MOC_LITERAL(10, 219, 6), // "action"
+QT_MOC_LITERAL(11, 226, 11), // "serviceName"
+QT_MOC_LITERAL(12, 238, 17), // "saveHistoryToFile"
+QT_MOC_LITERAL(13, 256, 30), // "on_pushButton_search_3_clicked"
+QT_MOC_LITERAL(14, 287, 30) // "on_pushButton_search_4_clicked"
 
     },
     "MainWindow\0on_pushButton_ajouter_clicked\0"
     "\0on_pushButton_supprimer_clicked\0"
     "on_pushButton_update_clicked\0"
     "on_pushButton_search_clicked\0extractToPdf\0"
-    "on_pushButton_refresh_clicked"
+    "on_pushButton_refresh_clicked\0"
+    "on_pushButton_search_2_clicked\0"
+    "addToHistory\0action\0serviceName\0"
+    "saveHistoryToFile\0on_pushButton_search_3_clicked\0"
+    "on_pushButton_search_4_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -55,7 +66,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,17 +74,29 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    0,   47,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    2,   81,    2, 0x08 /* Private */,
+       9,    1,   86,    2, 0x28 /* Private | MethodCloned */,
+      12,    0,   89,    2, 0x08 /* Private */,
+      13,    0,   90,    2, 0x08 /* Private */,
+      14,    0,   91,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   10,   11,
+    QMetaType::Void, QMetaType::QString,   10,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -93,10 +116,15 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->on_pushButton_search_clicked(); break;
         case 4: _t->extractToPdf(); break;
         case 5: _t->on_pushButton_refresh_clicked(); break;
+        case 6: _t->on_pushButton_search_2_clicked(); break;
+        case 7: _t->addToHistory((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 8: _t->addToHistory((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 9: _t->saveHistoryToFile(); break;
+        case 10: _t->on_pushButton_search_3_clicked(); break;
+        case 11: _t->on_pushButton_search_4_clicked(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -124,13 +152,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 12;
     }
     return _id;
 }
